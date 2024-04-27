6a96862cbe8b0944e30d72a4034da36062279cc7
amjidkhani <amjidkhaniuoh@gmail.com>
2024-04-27 20:31:09 +0500

    splash Screen Added

---
+++ lib/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Splash Screen"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              height: 300,
+              width: 300,
+              color: Colors.red,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
