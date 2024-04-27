e9ccfc650f42f790422103b78c8f86c2bc0062e6
amjidkhani <amjidkhaniuoh@gmail.com>
2024-04-27 21:41:58 +0500

    Video_Calling _Feature Added

---
+++ lib/video_calling/video_calling_feature.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class VideoCallingFeature extends StatefulWidget {
+  const VideoCallingFeature({super.key});
+
+  @override
+  State<VideoCallingFeature> createState() => _VideoCallingFeatureState();
+}
+
+class _VideoCallingFeatureState extends State<VideoCallingFeature> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Video Calling"),
+      ),
+    );
+  }
+}
