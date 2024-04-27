3e4c7fd950336de870dec261c766489569148cd0
AtifKhani <atiffk309@gmail.com>
2024-04-27 20:40:07 +0500

    Added Home Screen

---
+++ lib/home_screen].dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("Home Screen"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(child: Text("Home Screen"))
+        ],
+      ),
+    );
+  }
+}
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_screen].dart';
+
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -8,6 +10,15 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+  Future.delayed(const Duration(seconds: 3)).then((value) {
+
+Navigator.push(context, MaterialPageRoute(builder:(_)=>const HomeScreen()));
+
+  });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
